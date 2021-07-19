f905ca01096017269cfcf056305fcd3b1f7b7e95
Mateusz Konopka <mkonopkadev@gmail.com>
2021-07-19 23:47:52 +0200

    tt

--- src/home.js
+++ src/home.js
@@ -1,13 +1,28 @@
+import { useState, useEffect } from 'react';
+import BlogList from './blogList';
+
 const Home = () => {
 
-    const handleClick = () => {
-        console.log('Hello REACT');
+    const [blogs, setBlogs] = useState([
+        {title: 'Blog 1', body: 'bla bla 1', author: 'LS', id: 1},
+        {title: 'Blog 2', body: 'bla bla 2', author: 'LS', id: 2},
+        {title: 'Blog 3', body: 'bla bla 3', author: 'MK', id: 3}
+    ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('useEffect ran'); 
+    });
+
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click Me</button>
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'LS')} title="LS's blogs" /> */}
         </div>
      );
 }
